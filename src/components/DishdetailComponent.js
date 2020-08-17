b3d2da5ef9282183a07198b509597bdcfc06d383
luz adriana <luzadri.2908@gmail.com>
2020-08-17 11:01:28 -0400

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, BreadcrumbItem, Breadcrum
      Button, Modal, ModalHeader, ModalBody, Row, Label, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -141,11 +142,26 @@ class CommentForm extends Component{
 
     const DishDetail = (props) => {
 
-        let el = <div></div>;
-
-
-        if(props.dish){
-            el = <div className="container">
+        if(props.isLoading){
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading/>
+                    </div>
+                </div>
+            );
+        }else if(props.errMess){
+            return(
+                <div className="container">
+                    <div className="row">
+                         <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if(props.dish != null){
+            return(
+            <div className="container">
                 <div className="row"> 
                     <Breadcrumb>
                         <BreadcrumbItem>
@@ -168,8 +184,13 @@ class CommentForm extends Component{
                          </div>    
                     </div>
                 </div>
+            );
+        }else{
+            return(
+                <div></div>
+            );
         }
-        return(el);
+        
      
     }
 
